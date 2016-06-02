0467ccf82136de94d6018454fdad9266bf951011
陳俊賢 <vest12385@hotmail.com>
2016-06-02 23:15:14 +0800

    security & secret input

--- restaurant/main.c
+++ restaurant/main.c
@@ -11,6 +11,8 @@ int main(void)
 	struct tm * now = localtime(&current_time);
 	int adults = 0,
 		childs = 0,
+		preturn = 0,
+		c,
 		year = now->tm_year + 1900,
 		month = now->tm_mon + 1,
 		day = now->tm_mday,
@@ -19,16 +21,45 @@ int main(void)
 		second = now->tm_sec,
 		wday = now->tm_wday;
 	//printf("%d %d %d %d %d %d %d", year, month, day, hour, minute, second, wday);
+	printf("請入大人人數：");
+	while ((preturn = scanf("%d", &adults)) != 1)
+	{
+		printf("請輸入數字!!!! 請入大人人數：\n");
+		while ((c = getchar()) != '\n' && c != EOF); //clear stdin buffer, because if your input was not integer scanf didn't accecpt , then the input stay live in buffer
+	}
+	if (adults == 830508)
+	{
+		printf("今天是星期幾?：");
+		while ((preturn = scanf("%d", &wday)) != 1)
+		{
+			printf("請輸入數字!!!! 今天是星期幾?：\n");
+			while ((c = getchar()) != '\n' && c != EOF); //clear stdin buffer, because if your input was not integer scanf didn't accecpt , then the input stay live in buffer
+		}
+		printf("現在時間是?：");
+		while ((preturn = scanf("%d", &hour)) != 1)
+		{
+			printf("請輸入數字!!!! 現在時間是?：\n");
+			while ((c = getchar()) != '\n' && c != EOF); //clear stdin buffer, because if your input was not integer scanf didn't accecpt , then the input stay live in buffer
+		}
+		printf("請入大人人數：");
+		while ((preturn = scanf("%d", &adults)) != 1)
+		{
+			printf("請輸入數字!!!! 請入大人人數：\n");
+			while ((c = getchar()) != '\n' && c != EOF); //clear stdin buffer, because if your input was not integer scanf didn't accecpt , then the input stay live in buffer
+		}
+	}
+	printf("請輸入小孩人數：");
+	while ((preturn = scanf("%d", &childs)) != 1)
+	{
+		printf("請輸入數字!!!! 請輸入小孩人數：\n");
+		while ((c = getchar()) != '\n' && c != EOF); //clear stdin buffer, because if your input was not integer scanf didn't accecpt , then the input stay live in buffer
+	}
 	if (hour >= 22 || hour <= 8)
 	{
 		printf("本餐廳尚未營業\n");
 		system("PAUSE");
 		exit(EXIT_FAILURE);
 	}
-	printf("請入大人人數：");
-	scanf("%d", &adults);
-	printf("請輸入小孩人數：");
-	scanf("%d", &childs);
 	if (adults + childs > 40)
 	{
 		printf("本餐廳沒那麼多位置！！\n");
@@ -37,7 +68,7 @@ int main(void)
 	}
 	int pay = countBill(hour, wday, adults, childs);
 	printf("---------------歡迎光臨---------------\n");
-	printf("   %d 年 %d 月 %d 號     %d 分 %d 秒  \n", year, month, day, hour, second);
+	printf("   %d 年 %d 月 %d 號     %d 點 %d 分 %d 秒  \n", year, month, day, hour, minute, second);
 	printf("   大人 %d 位  小孩 %d 位  共 %d 位\n", adults, childs, adults + childs);
 	printf("                         總共 %d 元\n", pay);
 	printf("---------------謝謝惠顧---------------\n");
